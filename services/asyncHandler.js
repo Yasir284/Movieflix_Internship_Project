c71520885a0f701cd6f0ba338878757386953275
Yasir284 <lambawalay@gmail.com>
2023-01-11 18:47:12 +0530

    added a service of async handler

---
+++ services/asyncHandler.js
@@ -0,0 +1,10 @@
+const asyncHandler = (fn) => async (req, res, next) => {
+  try {
+    await fn(req, res, next);
+  } catch (err) {
+    console.log(err);
+    res.status(err.code || 500).json({ success: false, message: err.message });
+  }
+};
+
+export default asyncHandler;
