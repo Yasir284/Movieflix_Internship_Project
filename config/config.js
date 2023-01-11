52ee8bef1532f88e2fffa1a46eefcaeb9cba9bc8
Yasir284 <lambawalay@gmail.com>
2023-01-11 19:27:00 +0530

    updated config

--- config/config.js
+++ config/config.js
@@ -6,6 +6,7 @@ const config = {
   PORT: process.env.PORT,
   MONGODB_URL: process.env.MONGODB_URL,
   JWT_SECRET: process.env.JWT_SECRET,
+  JWT_EXPIRY: process.env.JWT_EXPIRY || "30d",
 };
 
 export default config;
