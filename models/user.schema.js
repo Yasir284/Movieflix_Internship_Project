2a0278d87b63bdd22ab1bc8041cb57963694fa4d
Yasir284 <lambawalay@gmail.com>
2023-01-11 19:28:32 +0530

    added user schema with schema methods

---
+++ models/user.schema.js
@@ -0,0 +1,70 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+import JWT from "jsonwebtoken";
+import config from "../../../E-commerce project/config";
+import AuthRoles from "../../../E-commerce project/utils/authRoles";
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+      maxLength: [50, "Name must be less than 50 characters"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      validate: {
+        validator: validateEmail(email),
+        message: "E-mail is invalid",
+      },
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      maxLength: [8, "Password must be more than 8 characters"],
+      select: false,
+    },
+    role: {
+      type: String,
+      required: true,
+      enum: Object.values(AuthRoles),
+      default: AuthRoles.USER,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// Email validation function
+function validateEmail(email) {
+  let emailRegex =
+    /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+  return emailRegex.test(email);
+}
+
+// Encrypt password
+userSchema.pre("save", async function (next) {
+  if (!userSchema.isModified()) return;
+
+  this.password = await bcrypt.hash(this.password, 10);
+
+  next();
+});
+
+// Schema methods
+userSchema.methods = {
+  comparePassword: async function (enterPassword) {
+    return await bcrypt.compare(enterPassword, this.password);
+  },
+
+  getJwtToken: function () {
+    const payload = { _id: this._id, role: this.role };
+
+    JWT.sign(payload, config.JWT_SECRET, { expiresIn: config.JWT_EXPIRY });
+  },
+};
+
+export default mongoose.model("User", userSchema);
